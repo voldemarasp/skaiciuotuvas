44620aeca85843ff944efa6e730838570c2c4fc9
voldemarasp <voldemarasp@gmail.com>
2017-10-09 13:42:40 +0300

    paskutinis

--- index.php
+++ index.php
@@ -24,6 +24,32 @@
 			<div id="result" class="col">
 				none
 			</div>
+			<div class="col">
+				<button onclick="addrow()" class="btn-success">Prideti i lentele</button>
+			</div>
+			<div id="ratas1" class="col">
+				<img src="images/1.png">
+			</div>
+			<div id="ratas2" class="col">
+				<img src="images/2.gif">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col">
+<table class="table">
+  <thead>
+    <tr>
+      <th>Ratlankio skersmuo coliais</th>
+      <th>Padangos skersmuo milimetrais</th>
+      <th>Arkos dydis</th>
+      <th>Bendras CM</th>
+    </tr>
+  </thead>
+  <tbody id="lentele">
+
+  </tbody>
+</table>
+			</div>
 		</div>
 	</div>
 
--- script.js
+++ script.js
@@ -9,8 +9,36 @@ function calc() {
 	document.getElementById('result').innerHTML = suma;	
 
 	if (suma > trys) {
-		document.getElementById("result").style.color = "red"; 
+		document.getElementById("result").style.color = "red";
+		document.getElementById("ratas2").style.display = "none";
+		document.getElementById("ratas1").style.display = "block";
 	} else {
 		document.getElementById("result").style.color = "green"; 
+		document.getElementById("ratas1").style.display = "none";
+		document.getElementById("ratas2").style.display = "block";
 	}
+}
+
+
+function addrow() {
+
+	var lentele = document.getElementById("lentele");
+	var vienas = parseInt(document.getElementById('vienas').value);
+	var du = parseInt(document.getElementById('du').value);
+	var trys = parseInt(document.getElementById('trys').value || 0);
+	var keturi = (vienas * 2.54) + (du / 10 * 2);
+
+	/*table.innerHTML += "<tr><td>";*/
+	var tr = lentele.insertRow(0);
+	
+	var td0 = tr.insertCell(0);
+	var td1 = tr.insertCell(1);
+	var td2 = tr.insertCell(2);
+	var td3 = tr.insertCell(3);
+
+	td0.innerHTML = vienas;
+	td1.innerHTML = du;
+	td2.innerHTML = trys;
+	td3.innerHTML = keturi;
+
 }
